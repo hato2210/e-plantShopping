54920659ad08c46803857f18ba5fe8cda0d89859
Tony <ha.tony@outlook.com>
2025-01-27 01:12:13 -0500

    Fixed mistakes in task 2

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -15,13 +15,15 @@ export const CartSlice = createSlice({
           state.items.push({ name, image, cost, quantity: 1 });
         }
       },
-      state.items = state.items.filter(item => item.name !== action.payload);
+    removeItem: (state, action) => {
+        state.items = state.items.filter(item => item.name !== action.payload);
+    },
     updateQuantity: (state, action) => {
         const { name, quantity } = action.payload;
         const itemToUpdate = state.items.find(item => item.name === name);
         if (itemToUpdate) {
-        itemToUpdate.quantity = quantity;
-        }    
+          itemToUpdate.quantity = quantity;
+        }
     },
   },
 });
